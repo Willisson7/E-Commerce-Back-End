9b9ac26be9da918981be2d55c4a0afd9eedd8cda
[Grant Willis] <[thegwillis.gw@gmail.com]>
2023-03-26 16:11:34 -0700

    Completed Models

--- models/index.js
+++ models/index.js
@@ -5,12 +5,40 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsToMany(Category, {
+  through: {
+    model: Tag,
+    unique: false
+  },
+  // naming retrieved data
+  as: 'cat_product'
+})
 
 // Categories have many Products
+Category.hasMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  // naming retrieved data
+  as: 'product_Tag'
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  }
+})
 
 module.exports = {
   Product,
